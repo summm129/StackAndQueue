4c73f0ef3148729a9e13dc2539ed88ed150911bb
summm129 <97378571+summm129@users.noreply.github.com>
2023-02-19 01:34:54 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# StackAndQueue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jr5bp8)
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [BrowserModule, ReactiveFormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
