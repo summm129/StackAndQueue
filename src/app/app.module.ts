a85b07f250b610d323b3afff22ec068befc58d3e
summm129 <97378571+summm129@users.noreply.github.com>
2023-02-19 01:42:33 +0900

    不要なファイルを削除

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
